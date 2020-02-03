aec3ee65429db3eaede9e51677ef93565c26a6ef
Mohamed Jaffer S <jaffer-pt3136@jaffer-pt3136.csez.zohocorpin.com>
2020-02-03 17:01:34 +0530

    Version 3

---
+++ MyFrameworkMj/MyFrameworkMj/Second.swift
@@ -0,0 +1,15 @@
+//
+//  Second.swift
+//  MyFrameworkMj_mac
+//
+//  Created by Mohamed Jaffer S on 03/02/20.
+//
+
+import Foundation
+
+
+class Second {
+    static func printer() {
+        print("Inside second class")
+    }
+}
