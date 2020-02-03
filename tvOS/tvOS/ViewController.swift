caec393bdc9038f9abeea717200c444fca76aecc
Mohamed Jaffer S <jaffer-pt3136@jaffer-pt3136.csez.zohocorpin.com>
2020-02-03 13:25:46 +0530

    CH

---
+++ tvOS/tvOS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tvOS
+//
+//  Created by Mohamed Jaffer S on 03/02/20.
+//  Copyright © 2020 Mohamed Jaffer S. All rights reserved.
+//
+
+import UIKit
+import MyFrameworkMj
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        Service.printer()
+    }
+
+
+}
+
